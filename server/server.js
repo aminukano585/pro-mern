a9219207c9bc85eed9d31d850e4f91f5420053ec
Aminu Kano <aminukano585@gmail.com>
2019-11-07 07:19:03 +0100

    Add graphql support

--- server/server.js
+++ server/server.js
@@ -1,10 +1,43 @@
 import express from 'express';
+import apollo from 'apollo-server-express';
+const { ApolloServer } = apollo;
 
 const app = express();
 const port = 3000 || process.env.PORT;
 
+let aboutMessage = "Issue Tracker API v1.0";
+
+function setAboutMessage(_, { message }) {
+  return aboutMessage = message;
+}
+
+const typeDefs = `
+  type Query {
+    about: String!
+  }
+  type Mutation {
+    setAboutMessage(message: String!): String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    about: () => aboutMessage,
+  },
+  Mutation: {
+    setAboutMessage,
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 app.use(express.static('public'));
 
+server.applyMiddleware({ app, path: '/graphql' });
+
 app.listen(port, () => {
-  console.log('App started on port 3000');
+  console.log(`App started on port ${port}`);
 });
